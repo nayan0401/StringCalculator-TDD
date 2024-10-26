c55cb76e27595cb652b083d7bb44eb8546a6db51
Nayan Varma <deadpool7483@yahoo.com>
2024-10-26 12:57:06 +0530

    Added test cases for various scenarios

--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,15 +8,45 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
 
-    StringCalculator stringCalculator;
+    private StringCalculator stringCalculator;
 
     @BeforeEach
-    public void init(){
+    public void init() {
         stringCalculator = new StringCalculator();
     }
 
+    @AfterEach
+    public void terminate(){
+        stringCalculator = null;
+    }
+
     @Test
-    public void testEmptyString(){
+    public void testEmptyString() {
         assertEquals(0, stringCalculator.add(""));
+    }
+
+    @Test
+    public void testAddingOneNumber() {
+        assertEquals(7, stringCalculator.add("7"));
+    }
+
+    @Test
+    public void testAddingMultipleNumbers() {
+        assertEquals(10, stringCalculator.add("4,6"));
+    }
+
+    @Test
+    public void testNegativeNumber() throws IllegalArgumentException {
+        try {
+            String[] array = {"2", "-5"};
+            stringCalculator.sum(array);
+        } catch (IllegalArgumentException exception) {
+            assertEquals(exception.getMessage(), "Negative numbers not allowed: -5");
+        }
+    }
+
+    @Test
+    public void testDelimiters() {
+        assertEquals(7, stringCalculator.add("//;\n3;4"));
     }
 }
