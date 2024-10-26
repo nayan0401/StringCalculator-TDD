6e5c661c2dfd95ef4e0cfd09b53f0d1f1563cdaf
Nayan Varma <deadpool7483@yahoo.com>
2024-10-26 13:00:40 +0530

    Development code pushed for various scenarios

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -2,15 +2,55 @@ package org.example;
 
 public class StringCalculator {
 
-    public int add(String numbers) {
-        if (numbers.isEmpty()) {
-            return 0;
+    public int add(String numbers){
+        if(numbers.length() < 2){
+            if(numbers.isEmpty()){
+                return 0;
+            } else {
+                return convertStringToInteger(numbers);
+            }
         } else {
-            return convertStringToInteger(numbers);
+            String delimiter = ",";
+            if(numbers.matches("//(.*)\n(.*)")){
+                delimiter = Character.toString(numbers.charAt(2));
+                numbers = numbers.substring(4);
+            }
+
+            String[] listOfNumbers = splitNumbers(numbers, delimiter + "|\n");
+            return sum(listOfNumbers);
         }
     }
 
     private int convertStringToInteger(String numbers) {
         return Integer.parseInt(numbers);
     }
+
+    private String[] splitNumbers(String numbers, String delimiter) {
+        return numbers.split(delimiter);
+    }
+
+    public int sum(String[] listOfNumbers) {
+        int totalSum = 0;
+        StringBuilder negatives = new StringBuilder();
+
+        for (String number : listOfNumbers) {
+            int num = convertStringToInteger(number);
+            if (num < 0) {
+                if (negatives.length() == 0) {
+                    negatives.append(number);
+                } else {
+                    negatives.append(", ").append(number);
+                }
+            } else {
+                totalSum += num;
+            }
+        }
+
+        if (negatives.length() > 0) {
+            throw new IllegalArgumentException("Negative numbers not allowed: " + negatives);
+        }
+
+        return totalSum;
+    }
+
 }
